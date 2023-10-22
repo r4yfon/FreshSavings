4a55f25112a12a54ae62628f42844c71fe79eb9f
Egolution <crkee.2022@scis.smu.edu.sg>
2023-10-23 00:56:56 +0800

    images are now working
    for both prod desc and marketplace

--- freshsavings/src/pages/Marketplace.vue
+++ freshsavings/src/pages/Marketplace.vue
@@ -4,11 +4,15 @@ import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
 import 'bootstrap'; // Import Bootstrap JavaScript
 import axios from 'axios';
 
+
 </script>
+<style>body{background-color: white}.card{border:none}.product{background-color: #eee}.brand{font-size: 13px}.act-price{color:blue;font-weight: 700}.dis-price{text-decoration: line-through}.about{font-size: 14px}.color{margin-bottom:10px}label.radio{cursor: pointer}label.radio input{position: absolute;top: 0;left: 0;visibility: hidden;pointer-events: none}label.radio span{padding: 2px 9px;border: 2px solid #ff0000;display: inline-block;color: #ff0000;border-radius: 3px;text-transform: uppercase}label.radio input:checked+span{border-color: #ff0000;background-color: #ff0000;color: #fff}.btn-danger{background-color: #ff0000 !important;border-color: #ff0000 !important}.btn-danger:hover{background-color: #da0606 !important;border-color: #da0606 !important}.btn-danger:focus{box-shadow: none}.cart i{margin-right: 10px}</style>
+
 
 
 
 <template>
+  
   <section>
     <!-- Search bar -->
     <div class="input-group m-3">
@@ -37,7 +41,7 @@ import axios from 'axios';
         <div class="col mb-5" v-for="product of groceryItems" :key="product.iname">
           <div v-if="product.posting_status == 'Active'" class="card h-100">
             <!-- Product image-->
-            <img class="card-img-top" :src=imageurl(product.image) alt="..." />
+            <img class="card-img-top" :src=imageUrl(product.image) alt="..." />
             <!-- Product details-->
             <div class="card-body p-4">
               <div class="text-center">
@@ -54,7 +58,7 @@ import axios from 'axios';
                 <!-- Modal -->
 <div class="modal fade modal-lg" id="productDesc" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content"  style="background-color: rgba(0,0,0,.5);">
+      <div class="modal-content">
         <div class="modal-header" style="border-style: none;" >
         <div class="modal-body">
             <div class="container" id='PDescription'>
@@ -65,7 +69,7 @@ import axios from 'axios';
                             <div class="row">
                                 <div class="col-lg-6">
                                     <div class="images p-3">
-                                        <div class="text-center p-4"> <img id="main-image" :src="productimage" width="250" /> </div>
+                                        <div class="text-center p-4"> <img id="main-image" :src="productImageUrl()" width="250" /> </div>
                                         
                                     </div>
                                 </div>
@@ -129,6 +133,7 @@ export default {
   name: "marketplaceHome",
   data() {
     return {
+          
             qty: 1,
             max_qty: undefined,
             vendor: undefined,
@@ -137,7 +142,7 @@ export default {
             disPrice: undefined,
             discountPercentage: 80,
             expiry: undefined,
-            productimage: undefined,
+            productimage: "cabbage.webp",
             pid: 1,
 
       groceryItems: [],
@@ -189,8 +194,8 @@ export default {
             this.actPrice = dat.price;
             this.disPrice = dat.selling_price;
             this.expiry = dat.expiring_in;
+            this.productimage = dat.image;
             
-            this.image = "../assets/img/" + dat.image;
                }
 
             // qty: 1,
@@ -218,9 +223,11 @@ GetAllPostings(){
     console.log(error); 
 });
 },
-imageurl(img){ 
-  console.log("@/assets/img/" + img);
-  return "@/assets/img/" + img;
+imageUrl(img){ 
+  return require(`@/assets/img/${img}`);
+},
+productImageUrl(){ 
+  return require(`@/assets/img/${this.productimage}`);
 },
 },
   created() {   
@@ -236,5 +243,3 @@ input {
   margin-bottom: 0;
 }
 </style>
-<style>body{background-color: white}.card{border:none}.product{background-color: #eee}.brand{font-size: 13px}.act-price{color:blue;font-weight: 700}.dis-price{text-decoration: line-through}.about{font-size: 14px}.color{margin-bottom:10px}label.radio{cursor: pointer}label.radio input{position: absolute;top: 0;left: 0;visibility: hidden;pointer-events: none}label.radio span{padding: 2px 9px;border: 2px solid #ff0000;display: inline-block;color: #ff0000;border-radius: 3px;text-transform: uppercase}label.radio input:checked+span{border-color: #ff0000;background-color: #ff0000;color: #fff}.btn-danger{background-color: #ff0000 !important;border-color: #ff0000 !important}.btn-danger:hover{background-color: #da0606 !important;border-color: #da0606 !important}.btn-danger:focus{box-shadow: none}.cart i{margin-right: 10px}</style>
-
