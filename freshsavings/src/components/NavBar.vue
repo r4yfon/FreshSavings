3311c283a29b8dce21712b11e2a34337b3627152
hannahloh <hannahloh.2021@scis.smu.edu.sg>
2023-11-09 03:42:16 +0800

    created login to profile icon switch after login

--- freshsavings/src/components/NavBar.vue
+++ freshsavings/src/components/NavBar.vue
@@ -29,15 +29,8 @@
 
 
             <ul class=" navbar-nav ms-auto" style="padding-right: 30px; display: flex; align-items: center;">
-                <li class="nav-item" style="margin-right: 0px; margin-top: 1px;">
-                    <a v-if="isLoggedIn" class="nav-link" href="/checkout">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-cart"
-                            viewBox="0 0 16 16">
-                            <path
-                                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                        </svg>
-                    </a>
-                    <a v-else class="nav-link" href="/login">
+                <li class="nav-item" style="margin-right: 0px; margin-top: 1px;" v-if="isLoggedIn">
+                    <a class="nav-link" href="/checkout">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-cart"
                             viewBox="0 0 16 16">
                             <path
@@ -45,9 +38,16 @@
                         </svg>
                     </a>
                 </li>
+                
 
-                <li class="nav-item">
-                    <a class="nav-link" href="login">
+                <li class="login-item" v-if="!isLoggedIn">
+                    <a href="login" class="btn btn-success" style="margin-right: -30px;" role="button">
+                        Login
+                    </a>
+
+                </li>
+                <li class="nav-item" v-if="isLoggedIn">
+                    <a class="nav-link" href="profile">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-person"
                             viewBox="0 0 16 16">
                             <path
@@ -89,14 +89,12 @@ export default {
 
 
 
-
-
-
 </script>
 
 
 
 <style scoped>
+
 img {
     max-width: 100%;
     padding-left: 10%;
@@ -145,7 +143,7 @@ img {
     /* when cursor is hovered over, it appears from left to right in 0.5s */
 }
 
-.navbar ul li:hover::after {
+.navbar ul li:not(.login-item):hover::after {
     width: 100%;
 }
 
